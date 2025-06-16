006d3c0afb5c76e31a738b67ea9cc046c1f93b73
antonio <tonimaru81@gmail.com>
2025-06-16 02:28:04 +0200

    railway

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 
 use App\Http\Controllers\TipoController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,4 +14,10 @@ Route::get('/', function () {
 //Route::get('/tipos', [TipoController::class, 'getAll']);
 Route::get('/', function () {
     return 'Laravel funcionando 🎉';
+});
+Route::get('/clear-cache', function() {
+    Artisan::call('config:clear');
+    Artisan::call('config:cache');
+    Artisan::call('cache:clear');
+    return 'Cache limpiada y recacheada';
 });
